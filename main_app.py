904e04de467c9a9a90f81c64941da3080ad53c89
David Brown <brownd2@union.edu>
2017-07-14 12:29:39 -0400

    updated requirements

--- main_app.py
+++ main_app.py
@@ -1,8 +1,8 @@
 import flask
-#import user_attribution_v2
+import user_attribution_v2
 app = flask.Flask(__name__)
 @app.route("/")
 
 def index():
-	return('user-attribution-metrics')
+	return(user_attribution_v2)
 
